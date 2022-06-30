0e2bdf1a7d1a47cc86efc58234522207016b152a
Mateusz Malecki <mmalecki0620@gmail.com>
2022-06-30 19:21:41 +0200

    Add check if text already downloaded

--- app/scraping/scraping_songs.py
+++ app/scraping/scraping_songs.py
@@ -26,7 +26,7 @@ def get_links_to_songs(musician):
 
         if len(songs) > 1:
             for link in songs:
-                links_to_songs.append(link['href'])
+                links_to_songs.append((link['href'], link['title']))
             page_number += 1
         else:
             break
@@ -41,8 +41,6 @@ def get_text_of_song(url):
     song_page = get(f"https://www.tekstowo.pl{url}").content
     bs_song = BeautifulSoup(song_page, 'html.parser')
 
-    song_title = bs_song.find('div', class_="belka row mx-0 px-3")
-    song_title_clean = song_title.find('h1').get_text().translate((str.maketrans('', '', string.punctuation)))
     song_text_find = bs_song.find('div', class_="inner-text")
     if song_text_find:
         song_text = song_text_find.get_text()
@@ -50,21 +48,24 @@ def get_text_of_song(url):
             .replace('Zwrotka', '').replace('Refren', '')
     else:
         song_text_clean = '-'
-    return song_title_clean, song_text_clean
+    return song_text_clean
 
 
 def save_song_text(musician, title, text):
-    """
-    Saves song text to .txt file.
-    """
     with open(f'/app/files_songs/{musician}/{title}.txt', 'w') as file:
         file.write(text)
 
 
+def check_if_song_downloaded(musician, title):
+    directory = f'/app/files_songs/{musician}/'
+    file_list = os.listdir(directory)
+    for file in file_list:
+        if title in file:
+            return True
+    return False
+
+
 def get_songs_for_musician(musician):
-    """
-    Scraps and saves all songs of chosen musician.
-    """
     links = get_links_to_songs(musician=musician)
     number = 100
 
@@ -74,6 +75,11 @@ def get_songs_for_musician(musician):
         logger.info(f"[x] Folder for musician '{musician}' already exists.")
 
     for link in links:
-        title, text = get_text_of_song(link)
-        save_song_text(musician, f"{number} {title}", text)
-        number += 1
+        title = link[1].translate((str.maketrans('', '', string.punctuation)))
+        if check_if_song_downloaded(musician, title):
+            logger.info(f"[x] Song '{title}' already downloaded.")
+            number += 1
+        else:
+            text = get_text_of_song(link[0])
+            save_song_text(musician, f"{number} {title}", text)
+            number += 1
