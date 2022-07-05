e6a5fd5417ba0ac95780a5c49028d6a3cbf1f5fc
Mateusz Małecki <mateusz.malecki@borga.pl>
2022-07-05 15:54:10 +0200

    Deal with CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 import logging
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers.setup import router as setup_router
 from routers.rappers import router as rappers_router
@@ -17,6 +18,15 @@ logger.addHandler(handler)
 app = FastAPI()
 app.include_router(setup_router)
 app.include_router(rappers_router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
