0cd9b740e3727bf5f8fc5a97c110c60d8cdf7927
Mateusz Małecki <mateusz.malecki@borga.pl>
2022-07-08 10:11:18 +0200

    Fix flower, set crontab for task

--- app/celery_worker/celery.py
+++ app/celery_worker/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 BROKER_URL = os.environ.get("CELERY_BROKER_URL")
 BACKEND_URL = os.environ.get("CELERY_RESULT_BACKEND")
@@ -10,6 +11,13 @@ app = Celery(
     broker=BROKER_URL,
     include=['celery_worker.tasks']
 )
+
+app.conf.beat_schedule = {
+    "setup_musicians": {
+        "task": "setup_musicians",
+        "schedule": crontab(hour=0, minute=0, day_of_week='sunday')
+    }
+}
 
 if __name__ == '__main__':
     app.start()
